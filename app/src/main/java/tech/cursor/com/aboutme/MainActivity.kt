e0dec32166ddf1937f360e23e6f328c6c859bbc3
Yimesgen Morka <webDesigner299@gmail.com>
2018-12-04 09:20:59 +0100

    Initial commit

---
+++ app/src/main/java/tech/cursor/com/aboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.cursor.com.aboutme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
